3b1f53c01b4f8260df547a61463edd92795399cd
Menn_Ashraf <132656972+Menna-Ashraff@users.noreply.github.com>
2024-01-29 09:53:48 +0200

    Update formm.php

--- Lab3/formm.php
+++ Lab3/formm.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Form Validation Example</title>
+    <title>MENNA'S LAB3 FORM</title>
     <style>
         .error { color: red; }
     </style>
